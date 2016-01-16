6091b7f3b52fe2cb98fe8256c12deee7d5c12865
Satoshi Tanaka <tanarky@gmail.com>
2016-01-16 16:09:56 +0900

    Initial Commit

---
+++ MyRssReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyRssReader
+//
+//  Created by satoshi on 2016/01/16.
+//  Copyright © 2016年 tanarky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
